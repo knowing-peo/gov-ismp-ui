6037c44e976745807f705216b75eb26afa0dc203
knowing-peo <phkdmdm1@sina.com>
2018-08-06 22:49:48 +0800

    添加test文件

---
+++ src/test/Test.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="test">
+		<p>{{ msg }}</p>
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				msg:"test"
+			}
+		}
+	}
+</script>
+<style scoped></style>
